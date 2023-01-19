81a9e4e550c41cde52e94865151c77fd241eba81
vvictore <vanessavictorekpe@gmail.com>
2023-01-18 23:11:38 -0500

    Initial commit

---
+++ src/gitpractice/Gitpractice.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gitpractice;
+
+/**
+ *
+ * @author 16474
+ */
+public class Gitpractice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    int x = 5;
+    int y = 6;
+    int sum = x + y;
+System.out.println(sum);
+    }
+    
+}
